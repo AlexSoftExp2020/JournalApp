a58567e68837980edf36f7e33a9d4688d832dc0b
Alex <oleksii.pidhornyi@gmail.com>
2025-01-03 23:03:26 +0100

    add organizing views

---
+++ JournalApp/Challanges/OrganizingViews.swift
@@ -0,0 +1,23 @@
+//
+//  OrganizingViews.swift
+//  JournalApp
+//
+//  Created by Oleksii on 03.01.2025.
+//
+
+import SwiftUI
+
+struct OrganizingViews: View {
+    var body: some View {
+        Circle()
+        Circle()
+        //#-learning-code-snippet(2.containers)
+        //#-learning-code-snippet(4.containers)
+        //#-learning-code-snippet(5.containers)
+        //#-learning-code-snippet(6.containers)
+    }
+}
+
+#Preview {
+    OrganizingViews()
+}
