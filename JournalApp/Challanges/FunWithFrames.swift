058cf204080a6ddc69d7f22d4c9adb6958fa9539
Alex <oleksii.pidhornyi@gmail.com>
2025-01-03 22:49:58 +0100

    add fun with frames

---
+++ JournalApp/Challanges/FunWithFrames.swift
@@ -0,0 +1,24 @@
+//
+//  FunWithFrames.swift
+//  JournalApp
+//
+//  Created by Oleksii on 03.01.2025.
+//
+
+import SwiftUI
+
+struct FunWithFrames: View {
+    var body: some View {
+        HStack {
+            //#-learning-code-snippet(3.rectangle)
+            //#-learning-code-snippet(3.framesBasic)
+            //#-learning-code-snippet(3.maxWidth)
+        }
+        //#-learning-code-snippet(3.modifiersIntro)
+        //#-learning-code-snippet(3.maxWidthEffect)
+    }
+}
+
+#Preview {
+    FunWithFrames()
+}
