d312e02b760e466d6407ac637d0f9df656099b88
Alex <oleksii.pidhornyi@gmail.com>
2025-01-03 23:23:35 +0100

    add sizing view

---
+++ JournalApp/Challanges/SizingView.swift
@@ -0,0 +1,48 @@
+//
+//  SizingView.swift
+//  JournalApp
+//
+//  Created by Oleksii on 03.01.2025.
+//
+
+import SwiftUI
+
+struct SizingView: View {
+    var body: some View {
+        VStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundColor(.paleOrange)
+                    .frame(maxWidth: 200, maxHeight: 150)
+                VStack {
+                    Text("Roses are red")
+                    Image("Rose")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: 50)
+                        .foregroundColor(.themeRed)
+                    Text("violets are blue, ")
+                }
+            }
+            ZStack {
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundColor(.paleOrange).frame(maxWidth: 200, maxHeight: 150)
+                VStack {
+                    Text("I just love")
+                    Image("Heart")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: 50)
+                        .foregroundColor(.themeRed)
+                    Text("coding with you!")
+                }
+            }
+        }
+        .font(.headline)
+        .foregroundColor(.darkBrown)
+    }
+}
+
+#Preview {
+    SizingView()
+}
