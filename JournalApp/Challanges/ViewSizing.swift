649774df124daa4bf9296e9764fea3ab7b773aa3
Alex <oleksii.pidhornyi@gmail.com>
2025-01-03 23:28:42 +0100

    add view sizing

---
+++ JournalApp/Challanges/ViewSizing.swift
@@ -0,0 +1,20 @@
+//
+//  ViewSizing.swift
+//  JournalApp
+//
+//  Created by Oleksii on 03.01.2025.
+//
+
+import SwiftUI
+
+struct ViewSizing: View {
+    var body: some View {
+        HStack {
+            //#-learning-code-snippet(2.viewSizing)
+        }
+    }
+}
+
+#Preview {
+    ViewSizing()
+}
