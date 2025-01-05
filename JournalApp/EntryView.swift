b62776003d0d0e41980a0e0eee63e8ae76cce157
Alex <oleksii.pidhornyi@gmail.com>
2025-01-05 16:58:40 +0100

    extend entry view

--- JournalApp/EntryView.swift
+++ JournalApp/EntryView.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct EntryView: View {
+    @Binding var entry: Entry
+    @Binding var entryCopy: Entry
+    @Binding var isEditing: Bool
+    
+    @State private var showingCardOptions = false
+    @State private var showingSettings = false
+    
+    private func isSmallView(for entryRow: EntryRow) -> Bool {
+        return entryRow.cards[0].size == .small
+    }
+    
+    private var currentEntry: Entry {
+        isEditing ? entryCopy : entry
+    }
+    
+    private var currentEntryBinding: Binding<Entry> {
+        isEditing ? $entryCopy : $entry
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            Grid(alignment: .top) {
+                // MARK: TODO
+            }
+        }
     }
 }
 
