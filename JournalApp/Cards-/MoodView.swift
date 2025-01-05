bcb2fdbacfca61708eeb67c7b3a92fec9d441a6d
Alex <oleksii.pidhornyi@gmail.com>
2025-01-05 17:38:29 +0100

    add

---
+++ JournalApp/Cards-/MoodView.swift
@@ -0,0 +1,23 @@
+//
+//  MoodView.swift
+//  JournalApp
+//
+//  Created by Oleksii on 05.01.2025.
+//
+
+import SwiftUI
+
+struct MoodView: View {
+    @Binding var value: String
+    var isEditing: Bool
+    var fontStyle: JournalFont
+    var size: CardSize
+    
+    static var disableMoodViewFull = false
+    
+    var body: some View {
+        if size == .small {
+            // MARK: TODO MoodViewHalf
+        }
+    }
+}
