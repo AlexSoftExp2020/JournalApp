1aa8b469433e79de0fc854f870b38b183659c48a
Alex <oleksii.pidhornyi@gmail.com>
2025-01-10 01:14:30 +0100

    extend card view

--- JournalApp/CardView.swift
+++ JournalApp/CardView.swift
@@ -16,11 +16,23 @@ struct CardView: View {
     var body: some View {
         switch cardData.card {
         case .mood(let value):
-            // MARK: TODO
+            MoodView(value: Binding<String>(get: { value }, set: { cardData.card = .mood(value: $0) } ),
+                     isEditing: isEditing,
+                     fontStyle: fontStyle,
+                     size: cardData.size)
+        case .sleep(value: let value):
+            // MARK: TODO SleepView(...
+        case .sketch(value: let value):
+            // MARK: TODO SketchView(...
+        case .photo(value: let value):
+            // MARK: TODO PhotoView(...
+        case .text(value: let value):
+            // MARK: TODO TextView(...
         }
     }
 }
 
 #Preview {
-    CardView()
+    CardView(cardData: .constant(CardData(card: .mood(value: "😢"))), isEditing: true, fontStyle: .font1)
+        .background(CardBackground())
 }
