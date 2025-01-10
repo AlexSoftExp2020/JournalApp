ee90af812b35b6b154cb4ccbfbbd691311e1036c
Alex <oleksii.pidhornyi@gmail.com>
2025-01-10 20:39:00 +0100

    add sleep view half solution

--- JournalApp/PeviewsView.swift
+++ JournalApp/PeviewsView.swift
@@ -68,6 +68,34 @@ struct SleepViewHalfPreview: View {
     }
 }
 
+struct SleepViewHalfSolution: View {
+    @Binding var value: Double
+    var isEditing: Bool
+    var fontStyle: JournalFont
+    
+    var body: some View {
+        VStack {
+            Text(isEditing ? "How many hours did you sleep?" : "Hours Slept")
+                .foregroundColor(.darkBrown)
+                .font(fontStyle.uiFont(15))
+                .frame(maxWidth: .infinity, alignment: isEditing ? .leading : .center)
+            Spacer()
+            
+            Text("\(Int(value))")
+                .modifier(FontStyle(size: 50))
+            
+            Spacer()
+            
+            if isEditing {
+                Stepper("Hours Slept", value: $value, in: 0...12, step: 1)
+                    .labelsHidden()
+            }
+        }
+        .frame(minHeight: 100, maxHeight: 200)
+        .padding()
+    }
+}
+
 struct MoodViewFullSolution: View {
     @Binding var value: String
     var isEditing: Bool
