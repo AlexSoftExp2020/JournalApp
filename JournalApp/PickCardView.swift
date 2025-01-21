b2ecb560ef44c5eebe84dd9487054b42322aed28
Alex <oleksii.pidhornyi@gmail.com>
2025-01-21 20:42:45 +0100

    add pick card view

---
+++ JournalApp/PickCardView.swift
@@ -0,0 +1,58 @@
+//
+//  PickCardView.swift
+//  JournalApp
+//
+//  Created by Oleksii on 21.01.2025.
+//
+
+import SwiftUI
+
+struct PickCardView: View {
+    @Binding var entry: Entry
+    @Binding var showingSheet: Bool
+    
+    var body: some View {
+        VStack {
+            Grid(horizontalSpacing: 15, verticalSpacing: 15) {
+                GridRow {
+                    Color.clear.gridCellUnsizedAxes([.horizontal, .vertical])
+                    Text("Half")
+                        .modifier(FontStyle(size: 18))
+                    Text("Full")
+                        .modifier(FontStyle(size: 18))
+                }
+                
+                ForEach(Card.allCases, id: \.id) { option in
+                    GridRow {
+                        Text(Card.title(option))
+                            .modifier(FontStyle(size: 18))
+                            .gridCellAnchor(.trailing)
+                        Button {
+                            entry.addCard(card: CardData(card: option, size: .small))
+                            showingSheet = false
+                        } label: {
+                            // MARK: TODO CardOptionView
+                        }
+
+                    }
+                }
+            }
+        }
+        .padding(.top)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.paleOrange)
+        .overlay(alignment: .topTrailing) {
+            Button {
+                showingSheet.toggle()
+            } label: {
+                Image(systemName: "xmark")
+                    .modifier(FontStyle(size: 16))
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    PickCardView()
+}
