7cef2a98f4150fedd62e5808d696bd5b0b1a5d49
Alex <oleksii.pidhornyi@gmail.com>
2025-01-22 08:39:43 +0100

    add entry list

---
+++ JournalApp/EntryList.swift
@@ -0,0 +1,18 @@
+//
+//  EntryList.swift
+//  JournalApp
+//
+//  Created by Oleksii on 21.01.2025.
+//
+
+import SwiftUI
+
+struct EntryList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EntryList()
+}
